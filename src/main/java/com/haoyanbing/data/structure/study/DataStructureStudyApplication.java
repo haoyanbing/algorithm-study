e5f14de016b07a9cf37b46ed6bdffe8053022e7d
hao.yanbing <binary_hao@163.com>
2019-12-17 20:49:50 +0800

    init

---
+++ src/main/java/com/haoyanbing/data/structure/study/DataStructureStudyApplication.java
@@ -0,0 +1,13 @@
+package com.haoyanbing.data.structure.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureStudyApplication.class, args);
+    }
+
+}
